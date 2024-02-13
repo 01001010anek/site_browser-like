78dd96116a208a5a07f7095a08d128b1eb5f3377
Janek <158313193+01001010anek@users.noreply.github.com>
2024-02-13 18:21:12 +0100

    ver_2
    Działa, jest ładne, polecam

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+    openTab('o_mnie');
+});
+
 function openTab(tabName) {
-    var i, tabcontent;
+    var i, tabcontent, tablinks;
 
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+        tabcontent[i].classList.remove("active");
     }
 
-    document.getElementById(tabName).style.display = "block";
+    tablinks = document.getElementsByClassName("tab");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+
+    document.getElementById(tabName).classList.add("active");
+    document.querySelector("[onclick*='" + tabName + "']").classList.add("active");
+}
+
+function closeWindow() {
+    window.close();
+}
+
+function minimizeWindow() {
+    // Do something to minimize the window
+}
+
+function maximizeWindow() {
+    // Do something to maximize the window
 }
